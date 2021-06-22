3ef08b967ae83469ea2319d26c6327959840ddf0
unknown <antoniobatistajr@gmail.com>
2021-06-21 22:28:08 -0300

    Projeto PHP de introdução procedural

---
+++ php_intro/index.php
@@ -0,0 +1,38 @@
+<?php
+    session_name('sucesso');
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Intro</title>
+</head>
+<body>
+    <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+    <form action="script.php" method="post">
+        <p>
+        <?php 
+            $mensagemDeSucesso = isset($_SESSION['sucesso']) ? $_SESSION['sucesso'] : "";
+            if(!empty($mensagemDeSucesso)){
+                echo $mensagemDeSucesso;
+            }
+        ?>
+        </p>      
+        <p>Seu Nome: <input type="text" name="nome"></input></p>
+        <p>Sua Idade: <input type="text" name="idade"></input></p>
+        <p><input type="submit" value="Enviar dados competidor"/></p>
+    </form>
+
+    <?php
+        $mensagemDeErro = isset($_SESSION['erro']) ? $_SESSION['erro'] : "";
+        if(!empty($mensagemDeErro)){
+            echo $mensagemDeErro;
+        }
+    ?>
+</body>
+</html>
